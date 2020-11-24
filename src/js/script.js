4b995a06382700d2fbfe03b0ceb3686a97b94cb9
Fudo <dai.fudo@mail.ru>
2020-11-24 22:38:03 +0300

    First Commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,7 @@
 'use strict';
-//slider
 
-const slides=document.querySelectorAll('mySlides'),
-    prev = document.querySelector('.reviev__slider-prev'),
-    next = document.querySelector('.reviev__slider-next');
+    let button = document.getElementById('text');
 
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function showSlides(n){
-    if(n>slides.length){
-        slideIndex = 1;
-    }
-    if(n<1){
-        slideIndex=slideIndex.length;
-    }
-    slides.forEach(item => item.style.display='none');
-
-    slides[slideIndex-1].style.display='block';
-}
-function plusSlides(n){
-    showSlides(slideIndex+=n);
-}
- prev.addEventListener('click',()=>{
-    plusSlides(-1);
-});
- next.addEventListener('click',()=>{
-    plusSlides(1);
-});
+    function fix(){
+        button.style.color ='white';
+    }fix();
