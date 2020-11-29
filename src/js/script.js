e22212ad4d2992a2c3bdc06e1bd7fbbc560602f1
Fudo <dai.fudo@mail.ru>
2020-11-29 20:36:55 +0300

    Adaptive Fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
 'use strict';
 
-    let button = document.getElementById('text');
 
-    function fix(){
-        button.style.color ='white';
-    }fix();
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__menu'),
+    menuItem = document.querySelector('.header__menu_item'),
+    hamburger = document.querySelector('.hamburger'),
+    footer = document.querySelector(".footer");
+    footer.style.padding ='none';
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('header__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('header__menu_active');
+        });
+    });
+});
+
